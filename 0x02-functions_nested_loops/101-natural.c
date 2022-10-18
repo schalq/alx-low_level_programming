37c554a14f9dc3349c6a2c1c7638da35550bab8a
pascal <pascalejiofor44@gmail.com>
2022-10-18 16:27:52 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
 /**
- *main - Prints the add of the Fibonacci numbers
+ *main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int c;
-	long int n1, n2, fn;
+	int a, b;
 
-	n1 = 1;
-	n2 = 2;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 48; c++)
+	for (a = 1; a < 1024; a++)
 	{
-		fn = n1 + n2;
-		printf(", %ld", fn);
-		n1 = n2;
-		n2 = fn;
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
 	}
-	printf("\n");
+	printf("%d\n", b);
 	return (0);
 }
