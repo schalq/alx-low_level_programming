da3bb7babfe5dd3e87fcf4525a31282731087a1c
pascal <pascalejiofor44@gmail.com>
2022-10-26 16:57:46 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,12 @@
-Task 0
 #include "main.h"
 /**
- *  * _strcat - concatenates two strings,
- *   *
- *    * @dest: destination.
- *     * @src: source.
- *      * Return: the pointer to dest.
- *       *
- *        */
+ * _strcat - concatenates two strings,
+ *
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ *
+ */
 char *_strcat(char *dest, char *src)
 {
 	int count = 0, count2 = 0;
